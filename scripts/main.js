6fb0e9889fcdc0d0e632ca9312e9833246f38f79
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-10 12:29:32 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let slides = document.getElementsByClassName("slide");
+let btnRight = document.getElementById("button-right");
+let btnLeft = document.getElementById("button-left");
+let indexOfSlide = 1;
+const countOfSlides = slides.length;
+
+ShowImg(indexOfSlide);
+
+function ShowImg(n){
+  if(n < 1){
+    indexOfSlide = countOfSlides;
+  } else if(n > countOfSlides){
+    indexOfSlide = 1;
+  }
+  for (let i = 0; i <countOfSlides; i++) {
+    slides[i].style.display = 'none'; 
+  }
+ 
+  slides[indexOfSlide-1].style.display = 'block';
+}
+
+function plusSlides(n){
+  ShowImg(indexOfSlide += n);
+}
+
+btnLeft.onclick = function(){
+  plusSlides(-1);
+}
+btnRight.onclick = function(){
+  plusSlides(-1);
+}
